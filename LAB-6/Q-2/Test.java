b6a8107166d3951035650e68c5356289611a1d5c
Deepak <31294138+deepaksai395@users.noreply.github.com>
2017-09-11 19:56:17 +0530

    Add files via upload

--- LAB-6/Q-2/Test.java
+++ LAB-6/Q-2/Test.java
@@ -1,8 +1,22 @@
-// final keyword with variable
-class Test
-{static final int x=10;
-	public static void main(String [] args)
-	{          x=20;// output will be error because value of variable cannot be changed once declared final
-		System.out.println(x);
-	}
+// final keyword with method
+class A
+{
+ final  void print()    //output will be error  because the method "print()" is declared as final in class A 
+    {
+        System.out.println("hello world");
+    }
 }
+
+class Test extends A
+{ void print()
+    {
+        System.out.println("hello java ");
+    }
+   public static void main(String [] args)
+     {
+         Test obj = new Test();
+         obj.print();
+     }
+} 
+
+//remove the word final in class A, the code will run
