baea8c75384a5dd002de832d4eda54c57217b80e
Deepak <31294138+deepaksai395@users.noreply.github.com>
2017-09-11 23:56:00 +0530

    Add files via upload

---
+++ LAB-6/Q-3/M1.java
@@ -0,0 +1,12 @@
+package p1;
+public class M1  //class should be public to be accessed outside the package
+{
+	
+	int n=10;
+	private int pri=20;
+	public int pub=30;
+	protected int pro=40;
+
+	
+	
+}
---
+++ LAB-6/Q-3/Test.java
@@ -0,0 +1,14 @@
+package p2;
+import p1.*;
+class Test
+{
+	public static void main(String [] args)
+	{
+	   M1 m1=new M1();
+	
+	System.out.println(m1.n);
+	System.out.println(m1.pub);
+	System.out.println(m1.pri);
+	System.out.println(m1.pro);
+	}
+}
